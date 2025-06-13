d1fe9afd31f766c2c8bb23e32a8285d322035767
bb-de <113197046+bb-de@users.noreply.github.com>
2025-06-13 15:30:26 +0200

    Update analytics.ts

--- netlify/functions/analytics.ts
+++ netlify/functions/analytics.ts
@@ -1,9 +1,25 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 export async function handler(event: HandlerEvent, context: HandlerContext) {
+  const headers = {
+    'Access-Control-Allow-Origin': 'https://www.brandingbrothers.de', // Ersetzen Sie dies durch die genaue Domain, von der aus Sie Anfragen senden
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  };
+
+  // Handle Preflight-Anfragen (OPTIONS)
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers,
+      body: "", // Body für OPTIONS-Anfragen kann leer sein
+    };
+  }
+
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
+      headers,
       body: JSON.stringify({ message: "Method Not Allowed" }),
     };
   }
@@ -15,6 +31,7 @@ export async function handler(event: HandlerEvent, context: HandlerContext) {
   // Beispiel-Antwort für erfolgreichen Empfang
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify({ message: "Data received successfully!" }),
   };
 } 
