49eec5bdc39606aa96a563f45999901ce8e1ebeb
bb-de <vincent@brandingbrothers.de>
2025-06-18 23:07:37 +0200

    tts3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { Router, Route, Switch } from 'wouter';
+import { Router as WouterRouter, Route, Switch } from 'wouter';
 import { AccessibilityProvider } from '@/contexts/AccessibilityContext';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { Toaster } from '@/components/ui/toaster';
 import { WidgetDemo } from '@/pages/WidgetDemo';
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { useAccessibility } from "@/hooks/useAccessibility";
+import { SpeechControls } from "@/components/accessibility/SpeechControls";
+
+function AppRouter() {
+  return (
+    <Switch>
+      <Route path="/" component={WidgetDemo} />
+      <Route component={WidgetDemo} />
+    </Switch>
+  );
+}
 
 function App() {
+  const { settings } = useAccessibility();
   return (
-    <TooltipProvider>
-      <AccessibilityProvider>
-        <Router>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <WouterRouter>
           <div className="min-h-screen">
-            <Switch>
-              <Route path="/" component={WidgetDemo} />
-              <Route component={WidgetDemo} />
-            </Switch>
+            <AppRouter />
           </div>
-        </Router>
+        </WouterRouter>
         <Toaster />
-      </AccessibilityProvider>
-    </TooltipProvider>
+        {settings.textToSpeech && (
+          <div className="fixed bottom-5 right-[120px] z-[10000]">
+            <SpeechControls />
+          </div>
+        )}
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
 
